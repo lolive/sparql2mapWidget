73186263b93a41968ab310e7bb9725d1f6488fca
Olivier Rossel <github@lolive.net>
2013-06-28 16:42:21 +0200

    test.js parse a string mostly lowercased (except for GroundOverlay)

--- sparql2kml2googleMaps/test.js
+++ sparql2kml2googleMaps/test.js
@@ -4,9 +4,12 @@ myquery,
 'http://localhost:9005/sparql/',
 function(res){
  parseN3(res);
- var kmlString=(new XMLSerializer()).serializeToString(document.getElementById('kml'));
+ //var kmlString=(new XMLSerializer()).serializeToString(document.getElementById('kml'));
+ var kmlString=' <kml xmlns="http://earth.google.com/kml/3.2"> <document> <GroundOverlay data-qrepeat="GroundOverlay file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#GroundOverlay"> <icon data-qrepeat="icon GroundOverlay|file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#hasicon"> <href>http://localhost/TerraColor_Hong_Kong_15m_1024_64_kmz_L4_4_1.jpg</href> </icon> <latlonbox data-qrepeat="latlonbox GroundOverlay|file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#haslatlonbox"> <north data-qtext="html latlonbox|file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#north">22.20009844226043700</north> <south data-qtext="html latlonbox|file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#south">22.05796724226043500</south> <east data-qtext="html latlonbox|file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#east">113.67256904815704000</east> <west data-qtext="html latlonbox|file:///home/lolive/RDF2KML/hongKong.kml.pretty.n3#west">113.53043784815704000</west> </latlonbox> </GroundOverlay> </document> </kml>';
+
  parser.parseKmlString(kmlString);
+ //parser.parse(['kml/test.kml']);
 }
 );
 
-}
+});
